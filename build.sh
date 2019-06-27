eb2e6fcd5daa3fe43f03c3c03784b64cb6850741
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-28 01:44:39 +0900

    add validate arg

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+if [ $# -ne 1 ]; then
+  echo "specify tex file name" 1>&2
+  exit 1
+fi
+
 docker container run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex $1.tex
 docker container run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx $1.dvi
 open $1.pdf
